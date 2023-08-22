07b8e7517348a8ce6ea73bea9133827f663a19c2
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-22 14:34:37 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Switch } from 'react-native';
+import { ThemeContext } from './assets/src/context/ThemeContext';
 export default function App() {
   return (
     <View style={styles.container}>
